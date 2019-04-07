0ebd6908388ee723efe35955fee6ed694d43f659
Sadhanapotluri <Sadhanapotluri@gmail.com>
2019-04-07 20:12:56 +0530

    Create Interest.java

---
+++ Task2/simple_compound_interest/Interest.java
@@ -0,0 +1,9 @@
+package simple_compound_interest;
+/**
+ * This is an abstract class which can be extended by various classes.
+ */
+public abstract class Interest {
+	abstract double interest();
+	double principal,rate_of_interest,years;
+	
+}
