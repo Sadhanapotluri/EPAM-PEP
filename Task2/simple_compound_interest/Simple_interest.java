c304d16a67fb27f0ae2d691f2d559a3b05a712bc
Sadhanapotluri <Sadhanapotluri@gmail.com>
2019-04-07 20:14:41 +0530

    Create Simple_interest.java

---
+++ Task2/simple_compound_interest/Simple_interest.java
@@ -0,0 +1,9 @@
+package simple_compound_interest;
+/** 
+ * This program determines the simple interest for the given inputs.
+ */
+public class Simple_Interest extends Interest {
+	public double interest(){
+		return principal*rate_of_interest*years;
+	}
+}
